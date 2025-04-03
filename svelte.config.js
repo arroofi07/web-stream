e22824159cfe889a472cbdce7c95c7f8488ffc3b
arovi <arrooficr7@gmail.com>
2025-04-03 12:23:48 +0700

    update project to use @sveltejs/adapter-vercel and add related dependencies in package.json and pnpm-lock.yaml

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,8 @@ const config = {
 
 	kit: {
 		alias: {
-			"@/*": "./src/lib/*",
-			"$lib/*": "./src/lib/*"
+			'@/*': './src/lib/*',
+			'$lib/*': './src/lib/*'
 		},
 		adapter: adapter()
 	}
